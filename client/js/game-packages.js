ee6d4a51ecdad7826149fd611931d748e9e55862
Fredye <fry.andrade@gmail.com>
2017-10-06 02:01:44 -0500

    added funciton to manage the game packages data

--- client/js/game-packages.js
+++ client/js/game-packages.js
@@ -4,11 +4,27 @@ Game.packages = function() {
   });
 
   this.sendScore();
+  this.verifyPosition();
 }
 
 Game.sendScore = function() {
   this.socket.emit('scoreChange', {
     currentScore: 10,
     addition: 5
+  });
+}
+
+Game.verifyPosition = function() {
+  _this = this;
+  this.socket.on('positionChange', function(pack) {
+    /*console.log('x: ', position.x);
+    console.log('y: ', position.y);*/
+
+    _this.ctx.clearRect(0,0, _this.canvas.width, _this.canvas.height);
+    for(var i = 0; i < pack.length; i++) {
+      _this.ctx.fillRect(pack[i].x, pack[i].y, 30,30);
+    }
+    _this.ctx.strokeStyle =  "red";
+    _this.ctx.stroke();
   });
 }
