72da9f2776db3649135fb50c0929eda0c6575bc8
xfry <fry.andrade@gmail.com>
2017-10-08 00:02:45 -0500

    socket listener when user press some buttons.

--- app.js
+++ app.js
@@ -19,24 +19,52 @@ servr.listen(4000, () => {
 
 const io 					= require('socket.io')(servr, {});
 const socketList 	= require('./server/game/socket-list');
+const playerList 	= require('./server/game/player-list')
+const Player 			= require('./server/game/player');
 const gameLoop		= require('./server/game/game-loop');
 
 
 io.sockets.on('connection', socket => {
-	socket.id = Math.random();
-	socket.x 	= 0;
-	socket.y 	= 0;
+	socket.id 						= Math.random();
+	var player 						= Player(socket.id);
 	socketList[socket.id] = socket;
+	playerList[socket.id]	= player;
 
 	socket.on('scoreChange', score => {
 		console.log('the score has been saved: ', score.addition);
 	});
 
 	socket.on("disconnect", () => {
+		console.log('a player has been deisconnected: ', socketList[socket.id]);
 		delete socketList[socket.id];
+		delete playerList[socket.id];
 	});
 
-	gameLoop(socketList);
+	socket.on('keydown', positionData => {
+		if(positionData.input === 'right') {
+			playerList[socket.id].right = positionData.value;
+		} else if(positionData.input === 'left') {
+			playerList[socket.id].left = positionData.value;
+		} else if(positionData.input === 'up') {
+			playerList[socket.id].up = positionData.value;
+		} else if(positionData.input === 'down') {
+			playerList[socket.id].down = positionData.value;
+		}
+	});
+
+	socket.on('keyup', positionData => {
+		if(positionData.input === 'right') {
+			playerList[socket.id].right = positionData.value;
+		} else if(positionData.input === 'left') {
+			playerList[socket.id].left = positionData.value;
+		} else if(positionData.input === 'up') {
+			playerList[socket.id].up = positionData.value;
+		} else if(positionData.input === 'down') {
+			playerList[socket.id].down = positionData.value;
+		}
+	});
+
+	gameLoop(playerList);
 
 });
 
