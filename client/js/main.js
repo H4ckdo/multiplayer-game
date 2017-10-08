9825af40581f2f0388024787887f11215ee59f9b
xfry <fry.andrade@gmail.com>
2017-10-08 00:04:37 -0500

    refactor

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,23 @@
-requirejs.config({
-    baseUrl: 'client/js',
-    paths: {
-      io: '/vendors/socket-io'     
-    }
-});
+document.onload = (function(){
+    requirejs.config({
+        baseUrl: 'client/js',
+        paths: {
+          io: '/vendors/socket-io'     
+        }
+    });
 
-// Start the main app logic.
-requirejs([
-            'client/js/vendors/socket-io.js',
-            'client/js/game.js',
-            'client/js/game-packages.js',
-            'client/js/player.js',
-          ],
-    
-    function (io, game, gamePackages) {
-        Game.start();
-        Game.socket = io();
-        Game.packages();
-    }
-);
+    // Start the main app logic.
+    requirejs([
+                'client/js/vendors/socket-io.js',
+                'client/js/game.js',
+                'client/js/game-packages.js',
+                'client/js/player.js',
+              ],
+        
+        function (io, game, gamePackages) {
+            Game.start();
+            Game.socket = io();
+            Game.packages();
+        }
+    );
+})();
